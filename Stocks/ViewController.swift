c93f3cbd091c43d707f2e1a310f4c52e2d97b27a
griwski <griwski@gmail.com>
2020-11-06 13:42:39 +0300

    add uiPickerView and constraints to right uiLabel

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var companyNameLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        companyNameLabel.text = "Tinkoff"
     }
 
 
