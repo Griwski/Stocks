1d67344647ee904e9e2205a7634a309d7d8bdd2f
griwski <griwski@gmail.com>
2020-11-10 09:13:30 +0300

    add delegates

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -7,19 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        companies.keys.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array(companies.keys)[row]
+    }
+    
+    
     @IBOutlet weak var companyNameLabel: UILabel!
     
     @IBOutlet weak var companyPickerView: UIPickerView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    private lazy var companies = [
+        "Apple" : "AAPL",
+        "Microsoft": "MSFT",
+        "Google": "GOOG",
+        "Amazon": "AMZN",
+        "Facebook": "FB"
+    ]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         companyNameLabel.text = "Tinkoff"
+        companyPickerView.dataSource = self
+        companyPickerView.delegate = self
     }
-
-
+    
+    
 }
 
 
